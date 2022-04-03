71a9920b6ffd9ee8737893f1d31a92fb96708380
wullxz <meik@schnitzelspecht.de>
2022-04-03 22:02:51 +0200

    faster expfarming and emptying the money every now and then

--- expfarm.js
+++ expfarm.js
@@ -1,10 +1,16 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	var farm = 'joesguns';
+	let maxmoney = JSON.parse(ns.read('servers.json.txt')).filter(x => x.hostname === 'joesguns')[0].moneyMax;
+	let farm = 'joesguns';
+	let counter = 0;
 	while (true) {
 		await ns.weaken(farm);
 		await ns.grow(farm);
-		await ns.weaken(farm);
-		await ns.sleep(100);
+		if (counter === 10 && ns.getServerMoneyAvailable(farm) > maxmoney * 0.9) {
+			await ns.hack(farm);
+			counter = 0;
+		}
+		counter++;
+		await ns.sleep(50);
 	}
 }
