ef8a720b828240e9ff8effae84fb7f156ca419c4
wullxz <meik@schnitzelspecht.de>
2022-04-04 14:28:02 +0200

    removed hack from exp script again

--- expfarm.js
+++ expfarm.js
@@ -1,16 +1,9 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	let maxmoney = JSON.parse(ns.read('servers.json.txt')).filter(x => x.hostname === 'joesguns')[0].moneyMax;
 	let farm = 'joesguns';
-	let counter = 0;
 	while (true) {
 		await ns.weaken(farm);
 		await ns.grow(farm);
-		if (counter === 10 && ns.getServerMoneyAvailable(farm) > maxmoney * 0.9) {
-			await ns.hack(farm);
-			counter = 0;
-		}
-		counter++;
-		await ns.sleep(50);
+		await ns.sleep(1);
 	}
 }
